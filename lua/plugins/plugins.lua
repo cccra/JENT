386abccaa0b9e22c4e6cc8cd23922635a8adfdc0
cccra <cccrg@talktalk.net>
2021-12-28 22:54:58 -0100

    plugin updates (small changes)

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -11,25 +11,18 @@ use {'wbthomason/packer.nvim',
 	config = function() require('plugins.plugins') end
 	}
 use {'nvim-telescope/telescope.nvim',-- Fuzzy find files
-	event = 'CmdlineEnter',
-	ft = 'norg',
-	config = function()
-		require('plugins.telescope')
-	end,
-	after = {'plenary.nvim', 'popup.nvim'},
-	requires = {'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim'}
+	keys = '<leader>f',
+	cmd = 'Telescope',
+	config = function() require('plugins.telescope') end,
+	requires = 'nvim-lua/plenary.nvim',
 }
 use {'TimUntersberger/neogit',-- Manage git within Vim
 	cmd = 'Neogit',
-	config = function()
-		require('plugins.neogit')
-	end
+	config = function() require('plugins.neogit') end
 }
 use {'numToStr/comment.nvim',-- Filetype-aware automatic commenting (gcc)
 	keys = 'gc',
-	config = function()
-	    require('Comment').setup()
-	end
+	config = function() require('Comment').setup() end
 }
 use {'machakann/vim-sandwich',-- Quickly surround text objects with parentheses, etc.
 	keys = 's'
@@ -45,39 +38,30 @@ use {'neovim/nvim-lspconfig',-- Language server linting and fixing
 use {'williamboman/nvim-lsp-installer',
 	cmd = {'LspInstallInfo', 'LspInstall', 'LspUninstall'},
 	after = 'nvim-lspconfig',
-	config = function()
-		require('plugins.nvim-lsp-installer')
-	end
+	config = function() require('plugins.nvim-lsp-installer') end
 }
 use {'hrsh7th/nvim-cmp',-- Autocompletion
-	event = 'InsertEnter',
 	ft = 'norg',
-	config = function()
-		require('plugins.nvim-cmp')
-	end
+	event = 'InsertEnter',
+	config = function() require('plugins.nvim-cmp') end
 }
 use {'hrsh7th/cmp-buffer', after = 'nvim-cmp'}
 use {'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp'}
 use {'hrsh7th/cmp-path', after = 'nvim-cmp'}
 use {'nvim-treesitter/nvim-treesitter',-- Extended syntax highlighting
 	run = ':TSUpdate',
+	-- this must remain commented until the neorg parser is included in treesitter
 	-- ft = {'python', 'sh', 'bash', 'tex', 'lua', 'cpp', 'bib', 'html', 'css', 'norg'},
-	config = function()
-		require('plugins.treesitter')
-	end
+	config = function() require('plugins.treesitter') end
 }
 use {'folke/zen-mode.nvim',-- Distraction-free mode (<leader>g)
 	cmd = 'ZenMode',
-	config = function()
-		require('plugins.zen-mode')
-	end
+	config = function() require('plugins.zen-mode') end
 }
 use {'norcalli/nvim-colorizer.lua',-- Highlight colour codes
 	ft = 'css',
 	cmd = 'ColorizerToggle',
-	config = function()
-		require('colorizer').setup{'css'}
-	end
+	config = function() require('colorizer').setup{'css'} end
 }
 use {'lukas-reineke/indent-blankline.nvim',-- Visual indentation guide
 	config = { "vim.g.indent_blankline_filetype_exclude = {'help', 'norg'}",
@@ -90,15 +74,11 @@ use {'simnalamburt/vim-mundo',-- Graphical undo
 }
 use {'folke/which-key.nvim',-- Key binding hints
 	keys = {'<leader>', '`', "'", '"', 'z', 'g'},
-	config = function()
-		require('plugins.which-key')
-	end
+	config = function() require('plugins.which-key') end
 }
 use { 'nvim-neorg/neorg',
-    config = function()
-        require('plugins.neorg')
-    end,
-	after = {'nvim-treesitter', 'nvim-cmp'}
+    config = function() require('plugins.neorg') end,
     requires = 'nvim-lua/plenary.nvim',
+	after = {'nvim-treesitter', 'nvim-cmp'},
 }
 end)
