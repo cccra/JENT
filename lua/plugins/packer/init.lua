b2c27a0abe61d53ae4d8c361c13505a72b85e0e3
cccra <cccrg@talktalk.net>
2021-12-30 16:45:39 +0000

    info while installing packer

--- lua/plugins/packer/init.lua
+++ lua/plugins/packer/init.lua
@@ -1,6 +1,7 @@
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	print('Installing Packer to manage plugins...')
 	INITIAL_INSTALL=vim.fn.system{'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path}
 	vim.cmd('packadd packer.nvim')
 end
