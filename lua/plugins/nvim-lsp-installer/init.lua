bfc382ae9f2dba5817b1062919e5c7efee57d8fe
cccra <cccrg@talktalk.net>
2022-02-04 22:26:51 -0100

    update lsp config

--- lua/plugins/nvim-lsp-installer/init.lua
+++ lua/plugins/nvim-lsp-installer/init.lua
@@ -22,14 +22,15 @@ for _, name in pairs(servers) do
 end
 
 local function on_attach(client, bufnr)
--- 	-- Set up buffer-local keymaps (vim.api.nvim_buf_set_keymap()), etc.
-      require 'illuminate'.on_attach(client)
+	-- Set up buffer-local keymaps (vim.api.nvim_buf_set_keymap()), etc.
+	require 'illuminate'.on_attach(client)
 end
 
 lsp_installer.on_server_ready(function(server)
 	-- Specify the default options which we'll use to setup all servers
 	local default_opts = {
 	on_attach = on_attach,
+	capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 	}
 
 	-- Now we'll create a server_opts table where we'll specify our custom LSP server configuration
