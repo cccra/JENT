385b7592bc2d263ad2a1675768769100a392d6a7
cccra <cccrg@talktalk.net>
2022-02-04 22:22:06 -0100

    change tabstop from 4 to 2

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,8 +1,8 @@
 vim.cmd[[filetype indent off]]
 vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
 vim.o.path = vim.o.path .. '**'
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.o.undofile = true
